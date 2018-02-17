aa9f52a1ae0cec7291c1628320cb1049fd611e15
Verthon <krzysiekksordyl@gmail.com>
2018-02-18 00:25:30 +0100

    added new component chat

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Chat from './Components/Chat';
 
 class App extends Component {
   render() {
---
+++ src/Components/Chat.js
@@ -0,0 +1,14 @@
+import react from 'react';
+
+class Chat extends Component{
+  render (){
+    return(
+      <div className="container">
+        <input type="text"/>
+        <button>send</button>
+      </div>
+    );
+  }
+}
+
+export default Chat;
